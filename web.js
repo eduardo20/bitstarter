13279a98672bb9c2b8d88ef33cf98077045f65d8
Ubuntu <ubuntu@ip-172-31-5-205.us-west-2.compute.internal>
2013-07-17 08:08:59 +0000

    web.js

--- web.js
+++ web.js
@@ -1,6 +1,12 @@
-#!/usr/bin/env node
-var fs = require('fs');
-var outfile = "hello.txt";
-var out = "Hello World 2!";
-//fs.writeFileSync(outfile, out);  
-console.log(out);
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
